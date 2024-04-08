0a9048f9f7a040a51eb43366563fbc25b934917e
Tom-Nightingale <tom.nightingale@mab.org.uk>
2024-04-08 16:48:51 +0100

    Added Entity Framework and created DB

---
+++ Data/HotelListingDbContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+
+    //This is adding it to the DB set
+        public DbSet<Hotel> Hotels {  get; set; }
+
+        public DbSet<Country>  Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "United Kingdom",
+                    ShortName = "UK",
+                }, new Country
+                {
+                    Id = 2,
+                    Name = "France",
+                    ShortName = "Fr"
+                }
+            );
+
+            modelBuilder.Entity<Hotel>().HasData(
+
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Hilton",
+                    CountryId = 1,
+                    Address = "1 Long Lane, London, UK",
+                    Rating = 5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Travel lodge",
+                    CountryId = 2,
+                    Address = "1 Short Road, Nottingham, UK",
+                    Rating = 6
+                }
+            );
+        }
+
+    }
+}
---
+++ Migrations/20240408153921_seededCountriesAndHotelsAgain.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotel_Listings.Migrations
+{
+    /// <inheritdoc />
+    public partial class seededCountriesAndHotelsAgain : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[] { 2, "1 Short Road, Nottingham, UK", 2, "Travel lodge", 6.0 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
